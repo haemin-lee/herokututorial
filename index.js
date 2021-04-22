ea2b08040040bda5bb53622ac1a2ddbb138743dd
haemin-lee <haeminle@usc.edu>
2021-04-21 18:47:48 -0700

    d

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express(); //create instance of this
 const port = process.env.port || 3000;
+const people = [];
 
 app.get("/", (req, res) => {
   console.log("get request");
-  res.send("Hello world");
+  res.json(people);
+  res.sendStatus(200);
 });
 
+app.post("/insert", (req, res) => {
+  people.push("Alex");
+  res.sendStatus(200);
+});
 app.listen(port, () => {
   console.log("hi there");
 }); //start this server on port 3000
